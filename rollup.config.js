139eb484a63819fb0ee2ce68aeb28c5b7147fcc3
hazen <hoyeungw@outlook.com>
2020-07-09 13:58:48 +0800

    minor fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import commonjs             from '@rollup/plugin-commonjs'
-import json                 from '@rollup/plugin-json'
-import nodeResolve          from '@rollup/plugin-node-resolve'
-import { decoObject, says } from '@spare/logger'
-import babel                from 'rollup-plugin-babel'
-import fileInfo             from 'rollup-plugin-fileinfo'
+import commonjs            from '@rollup/plugin-commonjs'
+import json                from '@rollup/plugin-json'
+import nodeResolve         from '@rollup/plugin-node-resolve'
+import { decoObject, ros } from '@spare/logger'
+import babel               from 'rollup-plugin-babel'
+import fileInfo            from 'rollup-plugin-fileinfo'
 
 const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
 
-console.log(says.roster('Executing'), name, process.cwd())
-console.log(says.roster('Dependencies'), decoObject(dependencies || {}, { bracket: true }))
+console.log(ros('Executing'), name, process.cwd())
+console.log(ros('Dependencies'), decoObject(dependencies || {}, { bracket: true }))
 
 const babelPluginOptions = {
   babelrc: false,
