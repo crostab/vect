1fe5451e0cd83da1ed7f0096705848932e8fd64f
hazen <hoyeungw@outlook.com>
2021-04-15 15:31:29 +0800

    update dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = function (api) {
   api.cache(true)
-  const presets = [['@babel/preset-env', { targets: { node: '10' } }]]
+  const presets = [['@babel/preset-env', { targets: { node: '14' } }]]
   const plugins = [
-    ['@babel/transform-runtime', { helpers: false }],
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    ['@babel/plugin-proposal-class-properties'],
-    ['@babel/plugin-proposal-private-methods'],
-    ['@babel/plugin-proposal-optional-chaining'],
-    ['@babel/plugin-proposal-nullish-coalescing-operator'],
+    [ '@babel/plugin-proposal-optional-chaining' ],
+    [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
+    [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
+    [ '@babel/plugin-proposal-class-properties', { loose: true } ],
+    [ '@babel/plugin-proposal-private-methods', { loose: true } ],
+    [ '@babel/plugin-transform-runtime', { helpers: false, } ]
   ]
   const ignore = ['node_modules/**']
   return {
--- rollup.config.js
+++ rollup.config.js
@@ -16,13 +16,14 @@ const babelPluginOptions = {
   sourceMap: true,
   babelHelpers: 'bundled',
   exclude: 'node_modules/**',
+  presets: [ [ '@babel/preset-env', { targets: { node: '14' } } ] ],
   plugins: [
-    ['@babel/plugin-transform-runtime', { helpers: false, }],
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-    ['@babel/plugin-proposal-nullish-coalescing-operator'],
-    ['@babel/plugin-proposal-class-properties'],
-    ['@babel/plugin-proposal-private-methods'],
-    ['@babel/plugin-proposal-optional-chaining']
+    [ '@babel/plugin-proposal-optional-chaining' ],
+    [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
+    [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
+    [ '@babel/plugin-proposal-class-properties', { loose: true } ],
+    [ '@babel/plugin-proposal-private-methods', { loose: true } ],
+    [ '@babel/plugin-transform-runtime', { helpers: false, } ]
   ]
 }
 
