e9f5a66495c0486ef434ff5ec6b1bc403739f05c
hazen <hoyeungw@outlook.com>
2022-04-14 10:05:05 +0800

    upgrade package manifest

--- packages/pkg-indicator/column-indicator/test/column-indicator.test.js
+++ packages/pkg-indicator/column-indicator/test/column-indicator.test.js
@@ -1,10 +1,12 @@
-import { randMatrix }   from '@foba/foo'
+import { roundD2 }      from '@aryth/math'
 import { delogger }     from '@spare/deco'
 import { logger }       from '@spare/logger'
 import { xr }           from '@spare/xr'
+import { init }         from '@vect/matrix'
 import { maxBy, minBy } from '../index'
 
-const matrix = randMatrix({ h: 6, w: 6 })
+const matrix = init(6, 6, (x, y) => roundD2(Math.sqrt(x ^ 2 + y ^ 2)))
+
 matrix |> delogger
 
 xr().max(maxBy(matrix, 0, x => x)).min(minBy(matrix, 0, x => x)) |> logger
--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,21 @@ import babel                           from '@rollup/plugin-babel'
 import commonjs                        from '@rollup/plugin-commonjs'
 import json                            from '@rollup/plugin-json'
 import nodeResolve                     from '@rollup/plugin-node-resolve'
-import { decoObject, decoString, ros } from '@spare/logger'
-import fileInfo                        from 'rollup-plugin-fileinfo'
+// import { decoObject, decoString, ros } from '@spare/logger'
+// import fileInfo                        from 'rollup-plugin-fileinfo'
 
-const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
+const { name, dependencies, main, module ,exports } = require(process.cwd() + '/package.json')
 
-console.log(ros('Executing'), name, decoString(process.cwd()))
-console.log(ros('Dependencies'), decoObject(dependencies || {}))
-
+// console.log(ros('Executing'), name, decoString(process.cwd()))
+// console.log(ros('Dependencies'), decoObject(dependencies || {}))
 
 export default [
   {
     input: 'index.js',
     external: Object.keys(dependencies || {}),
     output: [
-      { file: main, format: 'cjs' },  // CommonJS (for Node) build.
-      { file: module, format: 'esm' }  // ES module (for bundlers) build.
+      { file: exports['import'], format: 'esm' },  // ES module (for bundlers) build.
+      { file: exports['require'], format: 'cjs' }  // CommonJS (for Node) build.
     ],
     plugins: [
       nodeResolve({ preferBuiltins: true }),
@@ -32,17 +31,11 @@ export default [
           [ '@babel/preset-env', { targets: { node: '16' } } ]
         ],
         plugins: [
-          // ['@babel/plugin-proposal-decorators', { legacy: true }],
-          // [ '@babel/plugin-proposal-optional-chaining' ],
-          // [ '@babel/plugin-proposal-nullish-coalescing-operator' ],
           [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
-          // [ '@babel/plugin-proposal-class-properties', { loose: true } ],
-          // [ '@babel/plugin-proposal-private-methods', { loose: true } ],
-          // [ '@babel/plugin-transform-runtime', { helpers: false, } ]
         ]
       }),
       json(),
-      fileInfo(),
+      // fileInfo(),
     ]
   }
 ]
