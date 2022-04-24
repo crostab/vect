c8065f13782905df1a5dd70a35963c3f6501704c
hazen <hoyeungw@outlook.com>
2022-04-24 14:31:16 +0800

    minor fix

---
+++ packages/pkg-core/nested/dist/index.mjs
@@ -0,0 +1,65 @@
+const iterate = (nested, fn) => {
+  let row;
+  if (!nested) return;
+
+  for (let x in nested) {
+    if (!(row = nested[x])) continue;
+
+    for (let y in row) {
+      fn(row[y], x, y);
+    }
+  }
+};
+const iterateXY = (nested, onXY) => {
+  let row;
+  if (!nested) return;
+
+  for (let x in nested) {
+    if (!(row = nested[x])) continue;
+
+    for (let y in row) {
+      onXY(x, y);
+    }
+  }
+};
+const iterateY = (nested, onY) => {
+  let row;
+  if (!nested) return;
+
+  for (let x in nested) {
+    if (!(row = nested[x])) continue;
+
+    for (let y in row) {
+      onY(y);
+    }
+  }
+};
+const side = nested => {
+  return Object.keys(nested);
+};
+const head = nested => {
+  const vec = [];
+  iterateY(nested, y => {
+    if (vec.indexOf(y) < 0) vec.push(y);
+  });
+  return vec;
+};
+
+const nestedToRows = nested => {
+  const rows = [];
+  iterate(nested, (v, x, y) => {
+    rows.push([x, y, v]);
+  });
+  return rows;
+};
+
+const transpose = nested => {
+  const o = {};
+  iterate(nested, (v, x, y) => {
+    const xToV = o[y] ?? (o[y] = {});
+    xToV[x] = v;
+  });
+  return o;
+};
+
+export { head, iterate, iterateXY, iterateY, nestedToRows, side, transpose };
