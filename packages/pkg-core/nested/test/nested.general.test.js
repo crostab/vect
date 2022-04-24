800da36757980b7e6ecc5dbaa70a898315b9a7f5
hazen <hoyeungw@outlook.com>
2022-04-24 14:31:07 +0800

    add tests

---
+++ packages/pkg-core/nested/test/nested.general.test.js
@@ -0,0 +1,25 @@
+import { deco }                         from '@spare/deco'
+import { decoMatrix, logger, says, xr } from '@spare/logger'
+import { nestedToRows }                 from '../src/convert'
+import { head, side }                   from '../src/mapper'
+import { transpose }                    from '../src/transpose'
+
+const glyphToLayerToMetrics = {
+  A: { Lt: { l: 10, r: 10 }, Rg: { l: 8, r: 8 }, Bd: { l: 6, r: 6 } },
+  B: { Lt: { l: 36, r: 12 }, Rg: { l: 32, r: 10 }, Bd: { l: 30, r: 8 } },
+  C: { Lt: { l: 12, r: 12 }, Rg: { l: 10, r: 10 }, Bd: { l: 8, r: 8 } },
+  D: { Lt: { l: 36, r: 12 }, Rg: { l: 32, r: 10 }, Bd: { l: 30, r: 8 } },
+  E: { Lt: { l: 36, r: 12 }, Rg: { l: 32, r: 10 }, Bd: { l: 30, r: 8 } },
+  F: { Lt: { l: 36, r: 12 }, Rg: { l: 32, r: 10 }, Bd: { l: 30, r: 8 } },
+}
+
+xr().side(glyphToLayerToMetrics|> side).head(glyphToLayerToMetrics|> head) |> says['nested'].br('original')
+
+const layerToGlyphToMetrics = glyphToLayerToMetrics |> transpose
+xr().side(layerToGlyphToMetrics|> side).head(layerToGlyphToMetrics|> head) |> says['nested'].br('transposed')
+
+layerToGlyphToMetrics |> deco |> says['nested'].br('transposed')
+
+glyphToLayerToMetrics |> nestedToRows |> decoMatrix |> says['rows']
+
+
