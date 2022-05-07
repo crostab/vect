8ac00ebdd812c1a6728a5da9bcd428bd432f2058
hoyeungw <hoyeungw@outlook.com>
2022-05-07 09:00:53 +0800

    rename functions regarding indexed

--- packages/pkg-core/nested/src/indexed.js
+++ packages/pkg-core/nested/src/indexed.js
@@ -1,39 +1,41 @@
 import { FUN } from '@typen/enum-data-types'
 
-export function* simpleIndexed(nested) {
+function* indexedOf(nested) {
   let inner
   if (nested) for (let x in nested) {
     if ((inner = nested[x])) for (let y in inner) {
-      yield [x, y, inner[y]]
+      yield [ x, y, inner[y] ]
     }
   }
 }
 
-export function* filterIndexed(nested, filter) {
+function* indexedBy(nested, by) {
   let inner
   if (nested) for (let x in nested) {
     if ((inner = nested[x])) for (let y in inner) {
       const v = inner[y]
-      if (filter(x, y, v)) yield [x, y, v]
+      if (by(x, y, v)) yield [ x, y, v ]
     }
   }
 }
 
-export function* mappedIndexed(nested, mapper) {
+function* indexedTo(nested, to) {
   let inner
   if (nested) for (let x in nested) {
     if ((inner = nested[x])) for (let y in inner) {
-      yield mapper(x, y, inner[y])
+      yield to(x, y, inner[y])
     }
   }
 }
 
-export function* filterMappedIndexed(nested, filter, mapper) {
+function* indexed(nested, by, to) {
+  if (!by && !to) return yield* indexedOf(nested)
+  if (!to) return yield* indexedBy(nested, by)
   let inner
   if (nested) for (let x in nested) {
     if ((inner = nested[x])) for (let y in inner) {
       const v = inner[y]
-      if (filter(x, y, v)) yield mapper(x, y, v)
+      if (by(x, y, v)) yield to(x, y, v)
     }
   }
 }
@@ -44,19 +46,21 @@ export function* filterMappedIndexed(nested, filter, mapper) {
  * @param {function|{ [by]:function, to:function }} [conf]
  * @returns {Generator<*, void, *>}
  */
-export function* indexed(nested, conf) {
+function* indexedVia(nested, conf) {
   const by = conf?.by, to = conf?.to ?? conf
-  if (typeof by === FUN) {
-    if (typeof to === FUN) {
-      yield* filterMappedIndexed(nested, by, to)
-    } else {
-      yield* filterIndexed(nested, by)
-    }
-  } else {
-    if (typeof to === FUN) {
-      yield* mappedIndexed(nested, to)
-    } else {
-      yield* simpleIndexed(nested)
-    }
-  }
+  yield* typeof by === FUN
+    ? typeof to === FUN ? indexed(nested, by, to) : indexedBy(nested, by)
+    : typeof to === FUN ? indexedTo(nested, to) : indexedOf(nested)
+}
+
+export {
+  indexedOf,
+  indexedBy,
+  indexedTo,
+  indexed,
+  indexedVia,
+  indexedOf as simpleIndexed,
+  indexedBy as filterIndexed,
+  indexedTo as mappedIndexed,
+  indexedVia as filterMappedIndexed
 }
