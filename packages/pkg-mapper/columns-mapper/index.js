6231ba059134b0957a11b2e0ba066e97a9de91b1
hazen <hoyeungw@outlook.com>
2022-05-13 11:46:57 +0800

    minor fix

--- packages/pkg-mapper/columns-mapper/index.js
+++ packages/pkg-mapper/columns-mapper/index.js
@@ -1,2 +1,11 @@
-export { mapper, iterate }                          from './src/mapper'
-export { indexed, indexedBy, indexedOf, indexedTo } from './src/indexed'
+export {
+  mapper, mapper as columnsMapper,
+  iterate, iterate as columnsIterate,
+} from './src/mapper'
+
+export {
+  indexed, indexed as columns,
+  indexedBy, indexedBy as columnsBy,
+  indexedOf, indexedOf as columnsOf,
+  indexedTo, indexedTo as columnsTo,
+} from './src/indexed'
