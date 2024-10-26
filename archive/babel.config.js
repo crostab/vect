24de2a94f8211ebf291ecf7cef11d0fe341c62a7
hoyeungw <hoyeungw@outlook.com>
2024-10-26 22:46:37 +0800

    migrate from yarn + lerna to pnpm + @changesets

--- archive/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (api) {
-  api.cache(true)
-  const presets = [['@babel/preset-env', { targets: { node: '16' } }],]
-  const plugins = [['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],]
-  const ignore = ['node_modules/**']
-  return { presets, plugins, ignore }
-}
